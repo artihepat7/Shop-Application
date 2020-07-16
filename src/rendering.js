ff905bb102ddac68e315f8242d2b6553cadbc6f1
artihepat7 <hepatarti@gmail.com>
2020-07-16 22:56:04 +0530

    implementation

--- src/rendering.js
+++ src/rendering.js
@@ -1,20 +1,23 @@
-import { products } from "./product_management";
+//import { products } from "./product_management";
 
 export function rendering(products) {
   console.log("printing");
 
   const ul = document.getElementById("product-list");
-  ul.innerHTML = "";
   products.forEach((element) => {
-    ul.innerHTML =
-      ul.innerHTML +
-      `
-        <li>
-        <h2>${element.title}</h2>
-        <p>${element.price}</p>
-        <button>Delete</button>
-        </li>
-        
-        `;
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <h2>${element.title}</h2>
+          <p>${element.price}</p>
+      `;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    li.appendChild(deleteBtn);
+    ul.appendChild(li);
+
+    deleteBtn.addEventListener("click", (event) => {
+      console.log(element.id);
+      event.target.closest("li").remove();
+    });
   });
 }
